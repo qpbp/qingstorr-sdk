858ceebcf5d917a44bd8b871f482ee60e79f3ec6
ARoman <qpbd@ukr.net>
2016-06-16 20:24:54 +0300

    Implementing class

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-export function add(x, y) {
-  return x + y;
+class Qingstor{
+  constructor(zoneId = 'pek3a', secretKey, accessKey, options) {
+    this.zone = zoneId;
+    this.secretKey = secretKey;
+    this.accessKey = accessKey;
+    this.options = options;
+
+    console.log('here', this.zone, this.secretKey);
+  }
+
+  get message() {
+    return `${this.text} ${this.name}!`
+  }
+  set message(text) {
+    this.text = helpers.trim(text)
+  }
+
+  createBucket (name){
+
+  }
+
 }
+
+export default Qingstor;
+
---
+++ src/test.js
@@ -0,0 +1,5 @@
+import Qingstor from "./index.js";
+
+
+const a = new Qingstor();
+console.log(a);
